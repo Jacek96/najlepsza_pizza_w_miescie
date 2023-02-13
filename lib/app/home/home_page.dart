9509b763e68c49f7de00732d20c8548b9d8bbfde
Jacek Ferdynus <jacekferdynus96@gmail.com>
2023-02-13 18:54:00 +0100

    Install cloud firestore

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:najlepsza_pizza_w_miescie/app/home/add_opinion/add_opinion_page_content.dart';
+import 'package:najlepsza_pizza_w_miescie/app/home/my_account/my_account_page_content.dart';
+import 'package:najlepsza_pizza_w_miescie/app/home/restaurants/restaurants_paga_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -19,34 +22,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Najlepsza pizza we Wrocławiu'),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return Center(
-            child: Text('Jeden'),
-          );
+          return const RestaurantPageContent();
         }
         if (currentIndex == 1) {
-          return Center(
-            child: Text('Dwa'),
-          );
+          return const AddOpinionPageContent();
         }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jestes zalogowany jako ${widget.user.email}'),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Wyloguj'),
-              ),
-            ],
-          ),
-        );
+        return MyAccountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
